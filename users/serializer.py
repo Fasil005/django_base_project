774aed53dfbd006db1c7e214be92861d36c638f4
Fasil005 <fasilkeedath@gmail.com>
2021-12-29 20:48:47 +0530

    [ADD] added post and get code in view

--- users/serializer.py
+++ users/serializer.py
@@ -5,14 +5,16 @@ from .models import UserTable
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserTable
-        fields = ('id', 'username', 'email', 'password')
+        fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = UserTable(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
         )
-        user.set_password(validated_data['password'])
         user.save()
         return user
--- users/views.py
+++ users/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializer import UserSerializer
+from .models import UserTable
 
 
 class UserManagement(APIView):
     def get(self, request):
-        return Response(data={'message': 'Hello World'}, status=200)
+        try:
+            users = UserSerializer(UserTable.objects.all(), many=True)
+            return Response(data=users.data, status=200)
+        except Exception as e:
+            print(e)
+            return Response(data={'message': 'Something went wrong'}, status=500)
 
     def post(self, request):
         try:
-            return Response(data={'message': 'Success'}, status=201)
+            data = request.data
+            serializer = UserSerializer(data=data)
+            if serializer.is_valid():
+                serializer.create(data)
+                return Response(data={'message': 'User created successfully'}, status=200)
+            errors = ""
+            for i in serializer.errors:
+                errors += serializer.errors.get(i)[0].replace('This', i)
+                break
+            if errors:
+                return Response(data={'message': errors}, status=400)
         except Exception as e:
             print(e)
             return Response(data={'message': 'Something went wrong'}, status=500)
