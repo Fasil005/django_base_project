55c328f2f596ec1b0600f7d1d170f922119fe2f5
Fasil005 <fasilkeedath@gmail.com>
2021-12-29 17:51:15 +0530

    [ADD] added requirements.txt and serializer.py

--- users/views.py
+++ users/views.py
@@ -5,4 +5,11 @@ from rest_framework.response import Response
 
 class UserManagement(APIView):
     def get(self, request):
-        return Response({'message': 'Hello World'})
+        return Response(data={'message': 'Hello World'}, status=200)
+
+    def post(self, request):
+        try:
+            return Response(data={'message': 'Success'}, status=201)
+        except Exception as e:
+            print(e)
+            return Response(data={'message': 'Something went wrong'}, status=500)
