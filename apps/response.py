53d9e6f5f8efdd7f6a4f0a7e333255770da4be66
FasilFMS <muhammad.fasil@foundingminds.com>
2022-01-13 15:55:34 +0530

    [ADD],[EDIT] added success response, and edited all response(added msg as argument)

--- apps/response.py
+++ apps/response.py
@@ -2,63 +2,81 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-def response_fetched(data=None, count=None, errors=None):
+def response_success(data=None, count=None, errors=None, msg='Success'):
     return Response(data={
-        'status': 'Fetched',
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_200_OK)
 
 
-def response_created(data=None, count=None, errors=None):
+def response_fetched(data=None, count=None, errors=None, msg='Fetched'):
     return Response(data={
-        'status': 'Created',
+        'status': msg,
+        'errors': errors,
+        'count': count,
+        'data': data
+    }, status=status.HTTP_200_OK)
+
+
+def response_created(data=None, count=None, errors=None, msg='Created'):
+    return Response(data={
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_201_CREATED)
 
 
-def response_updated(data=None, count=None, errors=None):
+def response_updated(data=None, count=None, errors=None, msg='Updated'):
     return Response(data={
-        'status': 'Updated',
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_200_OK)
 
 
-def response_deleted(data=None, count=None, errors=None):
+def response_deleted(data=None, count=None, errors=None, msg='Deleted'):
     return Response(data={
-        'status': 'Deleted',
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_200_OK)
 
 
-def response_unprocessable_entity(data=None, count=None, errors=None):
+def response_not_found(data=None, count=None, errors=None, msg='Not Found'):
     return Response(data={
-        'status': 'Unprocessable Entity',
+        'status': msg,
+        'errors': errors,
+        'count': count,
+        'data': data
+    }, status=status.HTTP_204_NO_CONTENT)
+
+
+def response_unprocessable_entity(data=None, count=None, errors=None, msg='Unprocessable Entity'):
+    return Response(data={
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
-def response_bad_request(data=None, count=None, errors=None):
+def response_bad_request(data=None, count=None, errors=None, msg='Bad Request'):
     return Response(data={
-        'status': 'Bad Request',
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
     }, status=status.HTTP_400_BAD_REQUEST)
 
 
-def response_internal_server_error(data=None, count=None, errors=None):
+def response_internal_server_error(data=None, count=None, errors=None, msg='Internal Server Error'):
     return Response(data={
-        'status': 'Internal Server Error',
+        'status': msg,
         'errors': errors,
         'count': count,
         'data': data
